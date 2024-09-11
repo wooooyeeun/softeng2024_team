2d4bcb35c0e109e731905a55d251193a5fbc3077
wooooyeeun <153483777+wooooyeeun@users.noreply.github.com>
2024-09-11 23:38:35 +0900

    Add files via upload

--- hw03/all_gui.py
+++ hw03/all_gui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+from tkinter import messagebox
 
 from hw02.odd_even import is_even
 from hw02.prime import prime
@@ -7,141 +8,156 @@ from hw02.prime import prime
 
 def select():
     def odd_even():
-        n = int(ent_n.get())
-        if n % 2 == 0:
-            lbl_result["text"] = "짝수"
-        else:
-            lbl_result["text"] = "홀수"
+        try:
+            n = int(ent_n.get())
+            if n % 2 == 0:
+                lbl_result["text"] = "짝수"
+            else:
+                lbl_result["text"] = "홀수"
+        except ValueError:
+            messagebox.showerror("오류", "정수를 입력해주세요!")
 
     def is_prime():
-        n = int(ent_n.get())
-        if n < 2:
-            lbl_result["text"] = "소수가 아닙니다"
-        elif prime(n):
-            lbl_result["text"] = "소수입니다."
-        else:
-            lbl_result["text"] = "소수가 아닙니다."
+        try:
+            n = int(ent_n.get())
+            if n < 2:
+                lbl_result["text"] = "소수가 아닙니다"
+            elif prime(n):
+                lbl_result["text"] = "소수입니다."
+            else:
+                lbl_result["text"] = "소수가 아닙니다."
+        except ValueError:
+            messagebox.showerror("오류", "정수를 입력해주세요!")
 
     def prime_all():
-        n = int(ent_n.get())
-        prime_list = []
-        for i in range(1, n + 1):
-            if prime(i):
-                prime_list.append(i)
-        lbl_result["text"] = f"{prime_list}"
+        try:
+            n = int(ent_n.get())
+            prime_list = []
+            for i in range(1, n + 1):
+                if prime(i):
+                    prime_list.append(i)
+            lbl_result["text"] = f"{prime_list}"
+        except ValueError:
+            messagebox.showerror("오류", "정수를 입력해주세요!")
 
     def factorial():
-        n = int(ent_n.get())
-        result = 1
-        for i in range(1, n + 1):
-            result = result * i
-        lbl_result["text"] = f"{result}"
+        try:
+            n = int(ent_n.get())
+            result = 1
+            for i in range(1, n + 1):
+                result = result * i
+            lbl_result["text"] = f"{result}"
+        except ValueError:
+            messagebox.showerror("오류", "정수를 입력해주세요!")
 
     def sum_even():
-        n = int(ent_n.get())
-        total = 0
-        for i in range(1, n + 1):
-            if is_even(i):
-                total += i
-        lbl_result["text"] = f"{total}"
+        try:
+            n = int(ent_n.get())
+            total = 0
+            for i in range(1, n + 1):
+                if is_even(i):
+                    total += i
+            lbl_result["text"] = f"{total}"
+        except ValueError:
+            messagebox.showerror("오류", "정수를 입력해주세요!")
 
     def f2c():
-        temp_f = int(ent_n.get())
-        temp_c = (temp_f - 32) * 5 / 9
-        lbl_result["text"] = f"{temp_c:.2f} ℃"
+        try:
+            temp_f = int(ent_n.get())
+            temp_c = (temp_f - 32) * 5 / 9
+            lbl_result["text"] = f"{temp_c:.2f} ℃"
+        except ValueError:
+            messagebox.showerror("오류", "정수를 입력해주세요!")
 
-    def remove_result():
-        lbl_result["text"] = ""
 
     lbl_n = None
     btn_a = None
-
-
     frm_ent_n = tk.Frame(master=window)
     ent_n = tk.Entry(master=frm_ent_n, width=8)
 
-    frm_ent_n.grid(row=3, column=1, padx=10, pady=10)
-    ent_n.grid(row=3, column=1, padx=10, pady=10)
-
+    frm_ent_n.grid(row = 3, column = 1, sticky = "e")
+    ent_n.grid(row = 3, column = 1, sticky = "e")
 
     if combobox.get() == "홀짝구분":
-        lbl_n = tk.Label(window, text="<홀짝구분>", font=("gulim", 14))
+        lbl_n = tk.Label(window, text = "<홀짝구분>", font = ("gulim", 13, "bold"))
         btn_a = tk.Button(
-            master=window,
-            text="→",
-            width=6, height=1,
-            command=odd_even,
+            master = window,
+            text = "→",
+            width = 6, height = 1,
+            command = odd_even,
         )
-        btn_a.grid(row=3, column=2)
 
     elif combobox.get() == "소수판별":
-        lbl_n = tk.Label(window, text="<소수판별>", font=("gulim", 14))
+        lbl_n = tk.Label(window, text = "<소수판별>", font = ("gulim", 13, "bold"))
         btn_a = tk.Button(
-            master=window,
-            text="→",
-            width=6, height=1,
-            command=is_prime,
+            master = window,
+            text = "→",
+            width = 6, height = 1,
+            command = is_prime,
         )
 
     elif combobox.get() == "소수찾기":
-        lbl_n = tk.Label(window, text="<소수찾기>", font=("gulim", 14))
+        lbl_n = tk.Label(window, text = "<소수찾기>", font = ("gulim", 13, "bold"))
         btn_a = tk.Button(
-            master=window,
-            text="→",
-            width=6, height=1,
-            command=prime_all,
+            master = window,
+            text = "→",
+            width = 6, height = 1,
+            command = prime_all,
         )
 
     elif combobox.get() == "팩토리얼":
-        lbl_n = tk.Label(window, text="<팩토리얼>", font=("gulim", 14))
+        lbl_n = tk.Label(window, text = "<팩토리얼>", font = ("gulim", 13, "bold"))
         btn_a = tk.Button(
-            master=window,
-            text="→",
-            width=6, height=1,
-            command=factorial,
+            master = window,
+            text = "→",
+            width = 6, height = 1,
+            command = factorial,
         )
 
     elif combobox.get() == "짝수의합":
-        lbl_n = tk.Label(window, text="<짝수의합>", font=("gulim", 14))
+        lbl_n = tk.Label(window, text = "<짝수의합>", font = ("gulim", 13, "bold"))
         btn_a = tk.Button(
-            master=window,
-            text="→",
-            width=6, height=1,
-            command=sum_even,
+            master = window,
+            text = "→",
+            width = 6, height = 1,
+            command = sum_even,
         )
 
     elif combobox.get() == "온도변환":
-        lbl_n = tk.Label(window, text="<온도변환>", font=("gulim", 14))
+        lbl_n = tk.Label(window, text = "<온도변환>", font = ("gulim", 13, "bold"))
         btn_a = tk.Button(
-            master=window,
-            text="→",
-            width=6, height=1,
-            command=f2c,
+            master = window,
+            text = "→",
+            width = 6, height = 1,
+            command = f2c,
         )
+        lbl_temp_f = tk.Label(window, text = "℉")
+        lbl_temp_f.grid(row = 3, column = 1, sticky = "e")
 
-    lbl_result = tk.Label(master=window, text="\t")
-    lbl_result.grid(row=3, column=4, columnspan=2)
-    lbl_n.grid(row=2, column=1, pady=10, sticky="w")
-    btn_a.grid(row=3, column=2)
+    lbl_result = tk.Label(master = window, width = 20)
+    lbl_result.grid(row = 3, column = 3, sticky="w")
+    lbl_n.grid(row = 2, column = 2, pady = 10)
+    btn_a.grid(row = 3, column = 2, pady = 20)
+
 
 
 window = tk.Tk()
 window.title("GUI")
 window.geometry("350x200")
-window.resizable(width=False, height=False)
+window.resizable(width=True, height=True)
 
 lbl_select = tk.Label(window, text = "원하는 기능")
 select_list = ["팩토리얼", "홀짝구분", "소수판별", "소수찾기", "짝수의합", "온도변환"]
 combobox = ttk.Combobox(window, values = select_list)
-combobox.set("선택")
-combobox.grid(row = 1, column = 1, columnspan=3, padx = 10, pady = 10)
+combobox.set("원하는 기능 선택")
+combobox.grid(row = 1, column = 1, columnspan = 2, padx = 20, pady = 20, sticky = "w")
 btn = tk.Button(
         master=window,
         text = "선택",
         width = 5, height = 1,
         command = select,
 )
-btn.grid(row = 1, column = 4, padx = 10)
+btn.grid(row = 1, column = 3, sticky="w")
 
 
 window.mainloop()
