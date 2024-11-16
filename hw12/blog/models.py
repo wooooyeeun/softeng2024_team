2e98da4bd4571368c6552762bedcb796072dd0c1
wooooyeeun <153483777+wooooyeeun@users.noreply.github.com>
2024-11-17 03:58:44 +0900

    과제12 코드

---
+++ hw12/blog/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+import os
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d', blank=True)
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return f'{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
+
