62bafac3da5dd50c5d442ed1cffcb72df0d669b0
shinhye04 <153289428+shinhye04@users.noreply.github.com>
2024-12-22 23:36:06 +0900

    Add files via upload

---
+++ checklist/todo_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.todo_list, name='todo_list'),
+    path('delete-list/<int:list_id>/', views.delete_todo_list, name='delete_todo_list'),
+    path('add-item/<int:list_id>/', views.add_todo_item, name='add_todo_item'),
+    path('delete-item/<int:item_id>/', views.delete_todo_item, name='delete_todo_item'),
+    path('update-color/<int:list_id>', views.update_color, name='update_color'),
+]
