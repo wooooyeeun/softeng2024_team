bb3adf0b83ca99280034982fb253e47f48b3c8ba
wooooyeeun <153483777+wooooyeeun@users.noreply.github.com>
2024-12-23 06:05:48 +0900

    Add files via upload

---
+++ project/softeng2024_prj/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include('travel_page.urls')),
+    path('todo/', include('todo_app.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
